df83be67ff1c7eb81a60ff264e081d99d3f3fee8
marcosvillarreal <marcosevillarreal@gmail.com>
2017-05-24 12:18:06 -0300

    molino
    pasaje

--- Proyectos/molino/comprimir.bat
+++ Proyectos/molino/comprimir.bat
@@ -1,33 +1,7 @@
 @echo
 cls
-pkzip -ep -t  C:\Temporal\09101606.ges C:\Temporal\SECCION.tra
-if not errorlevel 0 goto Error
-pkzip -ep -t  C:\Temporal\09101606.ges C:\Temporal\MARCA.tra
-if not errorlevel 0 goto Error
-pkzip -ep -t  C:\Temporal\09101606.ges C:\Temporal\ARTICULO.tra
-if not errorlevel 0 goto Error
-pkzip -ep -t  C:\Temporal\09101606.ges C:\Temporal\CODBARRA.tra
-if not errorlevel 0 goto Error
-pkzip -ep -t  C:\Temporal\09101606.ges C:\Temporal\TABLAIVA.tra
-if not errorlevel 0 goto Error
-pkzip -ep -t  C:\Temporal\09101606.ges C:\Temporal\VENDEDOR.tra
-if not errorlevel 0 goto Error
-pkzip -ep -t  C:\Temporal\09101606.ges C:\Temporal\CATEGORI.tra
-if not errorlevel 0 goto Error
-pkzip -ep -t  C:\Temporal\09101606.ges C:\Temporal\ZONA.tra
-if not errorlevel 0 goto Error
-pkzip -ep -t  C:\Temporal\09101606.ges C:\Temporal\VALOR.tra
-if not errorlevel 0 goto Error
-pkzip -ep -t  C:\Temporal\09101606.ges C:\Temporal\PLAN.tra
-if not errorlevel 0 goto Error
-pkzip -ep -t  C:\Temporal\09101606.ges C:\Temporal\PLANCUE.tra
-if not errorlevel 0 goto Error
-pkzip -ep -t  C:\Temporal\09101606.ges C:\Temporal\PARAMETR.tra
-if not errorlevel 0 goto Error
-pkzip -ep -t  C:\Temporal\09101606.ges C:\Temporal\NEGOCIOS.tra
-if not errorlevel 0 goto Error
-pkzip -ep -t  C:\Temporal\09101606.ges C:\Temporal\DEPOSITO.tra
-if not errorlevel 0 goto Error
-goto salir
+pkzip -ep -t  J:\XSOFTMOLINO\PROYECTOS\MOLINO\Temporal\24051150.ges J:\XSOFTMOLINO\PROYECTOS\MOLINO\Temporal\SECCION.tra
+if not errorlevel 0 goto Salir
+pause
 :Error
 :Salir
